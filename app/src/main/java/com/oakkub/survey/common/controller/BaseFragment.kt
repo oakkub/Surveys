262628346fc6427f0d1b210b8f756255686fe528
Metas Kerdwat <metas.ker@ascendcorp.com>
2018-03-25 01:00:16 +0700

    Add utility function to help inflate layout in fragment

--- app/src/main/java/com/oakkub/survey/common/controller/BaseFragment.kt
+++ app/src/main/java/com/oakkub/survey/common/controller/BaseFragment.kt
@@ -1,10 +1,20 @@
 package com.oakkub.survey.common.controller
 
+import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 
 /**
  * Created by oakkub on 22/3/2018 AD.
  */
 abstract class BaseFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(layoutId, container, false)
+    }
+
+    abstract val layoutId: Int
 
 }
