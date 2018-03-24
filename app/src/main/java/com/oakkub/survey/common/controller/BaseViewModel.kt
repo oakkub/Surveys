6eea6c25f226eff47cf043a1610c76a8bb245481
Metas Kerdwat <metas.ker@ascendcorp.com>
2018-03-24 16:46:58 +0700

    Create BaseViewModel

---
+++ app/src/main/java/com/oakkub/survey/common/controller/BaseViewModel.kt
@@ -0,0 +1,17 @@
+package com.oakkub.survey.common.controller
+
+import android.arch.lifecycle.ViewModel
+import io.reactivex.disposables.CompositeDisposable
+
+/**
+ * Created by oakkub on 24/3/2018 AD.
+ */
+abstract class BaseViewModel : ViewModel() {
+
+    val disposable: CompositeDisposable = CompositeDisposable()
+
+    override fun onCleared() {
+        super.onCleared()
+        disposable.clear()
+    }
+}
