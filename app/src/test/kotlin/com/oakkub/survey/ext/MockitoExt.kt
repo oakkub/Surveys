84baa94618d604dc535e11b552fa5ee8e1db3cec
Metas Kerdwat <metas.ker@ascendcorp.com>
2018-03-24 13:44:42 +0700

    Create whenever extension function for Mockito.`when`(...)

---
+++ app/src/test/kotlin/com/oakkub/survey/ext/MockitoExt.kt
@@ -0,0 +1,9 @@
+package com.oakkub.survey.ext
+
+import org.mockito.Mockito
+import org.mockito.stubbing.OngoingStubbing
+
+/**
+ * Created by oakkub on 24/3/2018 AD.
+ */
+fun <T> whenever(methodCall: T?): OngoingStubbing<T> = Mockito.`when`(methodCall)
