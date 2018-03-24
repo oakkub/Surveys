8f1f3f85d70c4ddf8c694e2583fd4bf4be54e9a6
Metas Kerdwat <metas.ker@ascendcorp.com>
2018-03-24 13:23:45 +0700

    Create SurveyResponse

---
+++ app/src/main/java/com/oakkub/survey/data/services/surveys/SurveyResponse.kt
@@ -0,0 +1,13 @@
+package com.oakkub.survey.data.services.surveys
+
+import com.google.gson.annotations.SerializedName
+
+/**
+ * Created by oakkub on 24/3/2018 AD.
+ */
+data class SurveyResponse(
+        @SerializedName("id") val id: String,
+        @SerializedName("title") val title: String,
+        @SerializedName("description") val description: String,
+        @SerializedName("cover_image_url") val coverImageUrl: String
+)
