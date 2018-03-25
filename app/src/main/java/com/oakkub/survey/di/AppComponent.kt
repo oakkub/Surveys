08baa0a344c0c726df1e499aa088e2bdde80edd3
Metas Kerdwat <metas.ker@ascendcorp.com>
2018-03-25 13:35:57 +0700

    Import fresco image loader library and config it to use okHttp as network layer

--- app/src/main/java/com/oakkub/survey/di/AppComponent.kt
+++ app/src/main/java/com/oakkub/survey/di/AppComponent.kt
@@ -17,6 +17,7 @@ import dagger.android.support.AndroidSupportInjectionModule
             AndroidSupportInjectionModule::class,
             AppModule::class,
             AuthenticationModule::class,
+            FrescoModule::class,
             LocalModule::class,
             NetworkModule::class,
             RepositoryModule::class,
