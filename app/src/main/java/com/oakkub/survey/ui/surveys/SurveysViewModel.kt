a697a5dd287b33c818e46a4d3b76c02482c24e64
Metas Kerdwat <metas.ker@ascendcorp.com>
2018-03-25 11:20:32 +0700

    put @Inject to SurveysViewModel

--- app/src/main/java/com/oakkub/survey/ui/surveys/SurveysViewModel.kt
+++ app/src/main/java/com/oakkub/survey/ui/surveys/SurveysViewModel.kt
@@ -9,12 +9,13 @@ import com.oakkub.survey.data.services.oauth.OAuthRequest
 import com.oakkub.survey.data.services.surveys.SurveyRequest
 import com.oakkub.survey.data.services.surveys.SurveyResponse
 import com.oakkub.survey.extensions.plusAssign
+import javax.inject.Inject
 
 
 /**
  * Created by oakkub on 24/3/2018 AD.
  */
-class SurveysViewModel constructor(
+class SurveysViewModel @Inject constructor(
         private val oAuthRepository: OAuthRepository,
         private val surveysRepository: SurveysRepository,
         private val schedulerProvider: SchedulerProvider,
