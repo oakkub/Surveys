5b329cb939379fd256e0f0e20b9e8f8e7e73fc81
Metas Kerdwat <metas.ker@ascendcorp.com>
2018-03-25 07:21:21 +0700

    Add SurveysFragment to SurveysActivity

--- app/src/main/java/com/oakkub/survey/ui/surveys/SurveysActivity.kt
+++ app/src/main/java/com/oakkub/survey/ui/surveys/SurveysActivity.kt
@@ -3,11 +3,18 @@ package com.oakkub.survey.ui.surveys
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.oakkub.survey.R
+import kotlinx.android.synthetic.main.activity_surveys.*
 
 class SurveysActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_surveys)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .add(surveysRootContainer.id, SurveysFragment.newInstance())
+                    .commit()
+        }
     }
 }
