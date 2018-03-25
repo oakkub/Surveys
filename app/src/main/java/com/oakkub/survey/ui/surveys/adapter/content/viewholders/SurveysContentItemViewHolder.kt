ddbebed7348ba1325fd391f1162d5866b80f7493
Metas Kerdwat <metas.ker@ascendcorp.com>
2018-03-25 13:38:48 +0700

    Implement bind function of surveys content item

--- app/src/main/java/com/oakkub/survey/ui/surveys/adapter/content/viewholders/SurveysContentItemViewHolder.kt
+++ app/src/main/java/com/oakkub/survey/ui/surveys/adapter/content/viewholders/SurveysContentItemViewHolder.kt
@@ -7,6 +7,7 @@ import com.eggdigital.trueyouedc.extensions.views.inflate
 import com.oakkub.survey.R
 import com.oakkub.survey.ui.surveys.adapter.content.SurveysItemAdapterModel
 import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.item_surveys_content.*
 
 /**
  * Created by oakkub on 25/3/2018 AD.
@@ -14,7 +15,10 @@ import kotlinx.android.extensions.LayoutContainer
 class SurveysContentItemViewHolder private constructor(override val containerView: View) : RecyclerView.ViewHolder(containerView), LayoutContainer {
 
     fun bind(item: SurveysItemAdapterModel.Item) {
-
+        val response = item.surveyResponse
+        surveysItemCoverImageView.setImageURI(response.highResCoverImageUrl)
+        surveysItemTitleTextView.text = response.title
+        surveysItemDescriptionTextView.text = response.description
     }
 
     companion object {
